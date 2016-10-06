ffc7cd658fed3ac58ccac1367d1cc4a0e7c4abd6
Jessica Huynh <jhuynh37@brandeis.edu>
2016-10-06 14:57:04 -0400

    data folders done

--- data_prep.py
+++ data_prep.py
@@ -6,6 +6,7 @@ import sys
 
 zeroes = []
 ones = []
+
 for fn in os.listdir('waves_yesno'):
     if fn.startswith('0'):
         zeroes.append(fn)   # => training set
@@ -26,8 +27,13 @@ with open('data/train_yesno/text', 'w') as train_text, open('data/test_yesno/tex
     test_text.write(text(ones))
 
 # finish this method
-def wav_scp():
-    pass
+def wav_scp(filenames):
+    results = []
+    for filename in filenames:
+        basename = filename.split('.')[0]
+        results.append("{} {}".format(basename.split('.')[0], 'waves_yesno/'+filename))
+
+    return '\n'.join(sorted(results))
 
 with open('data/train_yesno/wav.scp', 'w') as train_text, open('data/test_yesno/wav.scp', 'w') as test_text:
     train_text.write(wav_scp(zeroes))
@@ -35,8 +41,13 @@ with open('data/train_yesno/wav.scp', 'w') as train_text, open('data/test_yesno/
 
 
 # finish this method
-def utt2spk():
-    pass
+def utt2spk(filenames):
+    results = []
+    for filename in filenames:
+        basename = filename.split('.')[0]
+        results.append("{} {}".format(basename.split('.')[0], 'global'))
+
+    return '\n'.join(sorted(results))
 
 with open('data/train_yesno/utt2spk', 'w') as train_text, open('data/test_yesno/utt2spk', 'w') as test_text:
     train_text.write(utt2spk(zeroes))
@@ -45,5 +56,14 @@ with open('data/train_yesno/utt2spk', 'w') as train_text, open('data/test_yesno/
 
 # finish this method
 # note that, spk2utt can be generate by using Kaldi util, once you have utt2spk file.
-def spk2utt():
-    pass
+def spk2utt(filenames):
+    os.system('utils/utt2spk_to_spk2utt.pl data/train_yesno/utt2spk > data/train_yesno/spk2utt')
+    os.system('utils/utt2spk_to_spk2utt.pl data/test_yesno/utt2spk > data/test_yesno/spk2utt')
+
+with open('data/train_yesno/spk2utt', 'w') as train_text, open('data/test_yesno/spk2utt', 'w') as test_text:
+    spk2utt(zeroes)
+    spk2utt(ones)
+
+# run fix script to prevent complaining about sorting
+os.system('utils/fix_data_dir.sh data/train_yesno/')
+os.system('utils/fix_data_dir.sh data/test_yesno/')
